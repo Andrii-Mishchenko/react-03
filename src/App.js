4030edfbb86c3b856a57b5b6ba3227de49769de0
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-06-18 14:27:41 +0300

    for check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
-import Counter from './components/Counter';
-import Dropdown from './components/Dropdown';
-import ColorPicker from './components/ColorPicker';
-import TodoList from './components/TodoList';
-import Form from './components/Form';
-import TodoEditor from './components/TodoEditor/TodoEditor';
-import Filter from './components/TodoList/Filter'
+// import Counter from './components/Counter';
+// import Dropdown from './components/Dropdown';
+// import ColorPicker from './components/ColorPicker';
+// import TodoList from './components/TodoList';
+// import Form from './components/Form';
+// import TodoEditor from './components/TodoEditor/TodoEditor';
+// import Filter from './components/TodoList/Filter'
+import Modal from './components/Modal';
+// import Clock from './components/Clock';
+import Tabs from './components/Tabs';
+import tabs from './tabs.json';
 
 import initialTodos from './todos.json';
 import shortid from 'shortid'
+
 
 const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
@@ -23,10 +28,25 @@ class App extends Component {
 
     state = {  
         todos: initialTodos,
-        filter: ''
-
+        filter: '',
+        showModal: false
     };
 
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.todos!==prevState.todos){
+            console.log("обновилось todos")
+            localStorage.setItem('todos', JSON.stringify(this.state.todos))
+        }
+    }
+
+    componentDidMount() {
+        const todos = localStorage.getItem('todos');
+        const parsedTodos = JSON.parse(todos);
+
+        if(parsedTodos){
+            this.setState({todos: parsedTodos})
+        }
+    }
 
     addTodo = text => {
         console.log(text)
@@ -85,25 +105,15 @@ class App extends Component {
         return todos.reduce((acc, todo) => (todo.completed ? acc+1 : acc), 0);
     }
 
-    componentDidUpdate(prevProps, prevState){
-        if(this.state.todos!==prevState.todos){
-            console.log("обновилось todos")
-            localStorage.setItem('todos', JSON.stringify(this.state.todos))
-        }
-    }
-
-    componentDidMount() {
-        const todos = localStorage.getItem('todos');
-        const parsedTodos = JSON.parse(todos);
-
-        if(parsedTodos){
-            this.setState({todos: parsedTodos})
-        }
+    toggleModal = () => {
+        this.setState(state =>({
+            showModal:!state.showModal
+        }))
     }
 
 
     render() { 
-        const {todos, filter} = this.state;
+        const {todos, filter, showModal} = this.state;
 
         const completedTodos = this.calculateCompletedTodos();
         
@@ -111,7 +121,30 @@ class App extends Component {
 
         return (    
             <>
-                <h1>Component`s state</h1>
+           {/* <Tabs items={tabs}></Tabs> */}
+            {/* <Clock></Clock> */}
+            <button type="button" onClick={this.toggleModal}>Открыть модалку</button>
+            {showModal && <Modal onClose={this.toggleModal}>
+                <h1>Контент модалки как children</h1>
+                <p>Мені тринадцятий минало;
+                    Я пас ягнята за селом.
+                    Чи то так сонечко сіяло,
+                    Чи так мені чого було –
+                    Мені так любо, любо стало,
+                    Неначе в Бога…
+                    Уже прокликали до паю,
+                    А я собі у бур’яні
+                    Молюся Богу, і не знаю,
+                    Чого маленькому мені
+                    Тоді так приязно молилось,
+                    Чого так весело було.
+                    Господнє небо і село,
+                    Ягня, здається, веселилось,
+                    І сонце гріло – не пекло.
+                </p>
+                <button type="button" onClick={this.toggleModal}>Закрыть</button>
+                </Modal>}
+                {/* <h1>Component`s state</h1>
                 
                 <Form onSubmit={this.formSubmitHandler}/>
                 <Counter initialValue={0}/>
@@ -132,7 +165,7 @@ class App extends Component {
                     <p>Общее кол-во: {todos.length}</p>
                    
                     <p>Кол-во выполненных todo: {completedTodos}</p>
-                </div>
+                </div> */}
             </> 
         );
     }
